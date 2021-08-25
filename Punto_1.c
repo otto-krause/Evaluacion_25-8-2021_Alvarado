8673f16c68374dc39bb61a1d80297faa4f3f3412
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-08-25 12:16:48 -0300

    Add files via upload

---
+++ Punto_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int Num=0, I=0;
+
+    printf("Ingrese un numero ");
+    scanf("%d",&Num);
+
+
+}
---
+++ Punto_2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int Dividendo=0, Divisor=0, Cociente=0, Resto=0, DividendoC=0, I=0;
+
+    printf("Ingrese el dividendo de la operacion ");
+    scanf("%d",&Dividendo);
+    printf("Ingrese el divisor de la operacion ");
+    scanf("%d",&Divisor);
+
+    if (Dividendo>Divisor && (Dividendo && Dividendo)>0)
+    {
+        do {
+            Cociente=Cociente+1;
+            Resto=Dividendo-Divisor;
+            Dividendo=Resto;
+            }
+        while(Resto>0);
+
+        printf("\nEl cociente de la division es %d", Cociente-1);
+        printf("\nEl resto de la division es %d", Resto);
+    }
+    else
+    {
+        printf("No se puede realizar la division");
+    }
+}
