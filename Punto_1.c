2d92d121429dd272191cfdbadfe592391143839e
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-08-31 20:42:21 -0300

    Update Punto_1.c

--- Punto_1.c
+++ Punto_1.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 int main()
 {
-    int Num=0, I=0;
+    int Num=0, I=1, NumF1=1, NumF2=1, NumF3=1;
+    printf("Ingrese el numero con el que desea realizar la sucesion: ");
+    scanf("%d", &Num);
 
-    printf("Ingrese un numero ");
-    scanf("%d",&Num);
+    switch (Num)
+    {
+    case 0:
+        puts("0");
+        break;
 
+    case 1:
+        puts("0, 1");
+        break;
+    
+    default:
+        for (I; I<=Num; I++)
+        {
+            NumF3=NumF1+NumF2;
+            NumF1=NumF2;
+            NumF2=NumF3;
 
+            printf("%d, ", NumF1);
+        }
+        break;
+    }
 }
